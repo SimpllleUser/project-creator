1832fa1d8dcfe7f40887b022584020172a9c2c7c
Vitalii.Pecherytsya <Vitalii.Pecherytsya@rgc.ua>
2022-04-04 20:36:15 +0300

    feat: add generator main import modules

--- generator-utils.js
+++ generator-utils.js
@@ -3,20 +3,21 @@ const { generateModel } = require('./generator-model');
 const { generateService } = require('./generator-service');
 const { gereneateRouters } = require('./generator-router');
 const { generateController } = require('./generator-controller');
+const { generateImports } = require('./genrator-import');
 
-const generateImports = arrayToLinesCode;
+// const generateImports = arrayToLinesCode;
 const generateInitImports = generateImports;
 const generateModuleExport = (json) => `export default ${JSONtoStringJSON(json)};`;
 const generateStaticCode = (string) => `${string}`;
 
 const genrateByKeyObject = {
     'imports': generateImports,
-    'init-module': generateImports,
+    'init-module': arrayToLinesCode,
     'export-default': generateModuleExport,
     'static-code': generateStaticCode,
-    'app-use': generateImports,
-    'constants': generateImports,
-    'route-modules': generateImports,
+    'app-use': arrayToLinesCode,
+    'constants': arrayToLinesCode,
+    'route-modules': arrayToLinesCode,
     'model': generateModel,
     'service': generateService,
     'routers': gereneateRouters,
